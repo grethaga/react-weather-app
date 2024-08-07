78a1d940642a31470c89633392b99eff88cf8555
grethagall <100848664+grethagall@users.noreply.github.com>
2024-08-07 14:19:06 +0200

    added axios, search engine, useState

---
+++ src/Weather.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import "./Weather.css";
+import Search from "./Search";
+import axios from "axios";
+
+export default function Weather() {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      temperature: Math.round(response.data.temperature.current),
+      wind: response.data.wind.speed,
+      city: response.data.city,
+      humidity: response.data.temperature.humidity,
+      description: response.data.condition.description,
+      icon: response.data.condition.icon_url,
+      date: "Friday 10:00",
+      ready: true,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <Search />
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
+        <div className="row">
+          <div className="col-6">
+            <img src={weatherData.icon} alt={weatherData.description} />
+            <span className="temp">{weatherData.temperature}</span>
+            <span className="unit">°C</span>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind}km/h</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    let apiKey = "o0d0713230t29ff3bf6accc034c782b3";
+    let city = "Vienna";
+    let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiURL).then(handleResponse);
+    return "Loading..";
+  }
+}
