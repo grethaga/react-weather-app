64e5edcf4c71d4f41be084076bb02396103022aa
grethaga <163857709+grethaga@users.noreply.github.com>
2024-08-20 16:24:25 +0200

    added forecast for one day

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,36 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
-    console.log(response.data);
+    console.log(response.data.daily);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
 
-  console.log(props);
-  let apiKey = "o0d0713230t29ff3bf6accc034c782b3";
-  let lon = props.data.longitude;
-  let lat = props.data.latitude;
-  let apiURL = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}&units=metric`;
-
-  axios.get(apiURL).then(handleResponse);
-
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="forecast-day">Thu</div>
-          <img
-            className="forecast-icon"
-            src={props.data.icon}
-            alt={props.data.description}
-          />
-          <div className="forecast-temp">
-            <span className="forecast-temp-max">19°</span>
-            <span className="forecast-temp-min">10°</span>
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "o0d0713230t29ff3bf6accc034c782b3";
+    let lon = props.data.longitude;
+    let lat = props.data.latitude;
+    let apiURL = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}&units=metric`;
+
+    axios.get(apiURL).then(handleResponse);
+
+    return null;
+  }
 }
