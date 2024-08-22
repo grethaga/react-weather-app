5110fdf92d8618b8af1ee188ffae50ba39a28a0b
grethaga <163857709+grethaga@users.noreply.github.com>
2024-08-22 10:49:44 +0200

    added forecast for 5 days

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function WeatherForecastDay(props) {
   function day() {
     let date = new Date(props.data.time * 1000);
-    let day = date.getDay() + 1;
+    let day = date.getDay();
 
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
